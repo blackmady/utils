33a40ca3f2d4aad80db92dcf49953708a3761233
arcoon <cg3385@163.com>
2019-11-07 18:00:58 +0800

    fix index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
 /*
  * @Author: None
  * @Date: 2019-11-05 15:40:34
- * @LastEditTime: 2019-11-06 15:06:27
+ * @LastEditTime: 2019-11-07 18:00:20
  * @LastEditors: None
  * @Description: 
  */
 import store from './store'
 import utils from './utils'
-export {base64ToBlob,compressImg,convertImgToBase64,getOrientation,previewImg,resetImgOrientation,resetOrientation} from './image'
+export {base64ToBlob,imageInfo,compressImg,convertImgToBase64,getOrientation,previewImg,resetImgOrientation,resetOrientation} from './image'
 export {store}
 export {filters} from './filters'
 export default utils
--- types/index.d.ts
+++ types/index.d.ts
@@ -1,6 +1,6 @@
 import store from './store';
 import utils from './utils';
-export { base64ToBlob, compressImg, convertImgToBase64, getOrientation, previewImg, resetImgOrientation, resetOrientation } from './image';
+export { base64ToBlob, imageInfo, compressImg, convertImgToBase64, getOrientation, previewImg, resetImgOrientation, resetOrientation } from './image';
 export { store };
 export { filters } from './filters';
 export default utils;
